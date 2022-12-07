aa3a839ea111f304ed37a38a5439057ae78bcf3a
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-07 15:12:44 +0100

    Impostato ciclo di stampa da 1 a 100

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('JS OK')
+/*
+Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz” al posto del numero.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+ */
+
+//Ciclo di stampa numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+}
